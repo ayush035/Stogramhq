35cffc5af51c4dc60e813a81ada66047615947db
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2023-05-22 22:26:16 +0530

    main

---
+++ stogram/components/Navbar.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+
+export default function Navbar() {
+
+    return (
+        
+            <>
+            <nav className=' flex justify-between h-12 text-green-400 bg-black   font-bold' >
+                <span className='mx-20 my-2 flex text-green-400 text-2xl  '><Link href={"/"}>Stogram</Link></span>
+                <ul className= 'px-2 py-3 flex space-x-10 mx-12 '>
+            {/* <div className='flex mx-16 '> */}
+              {/* <input className="px-4 h-8  rounded-lg  bg-gray-800 text-gray-400" type="text" placeholder="🔍 Search projects" /> */}
+        {/* </div> */}
+        
+        {/* <div className='hover:text-white delay-50  text-md font-semibold font-mono '>
+            <Link href="/">Home</Link></div> */}
+        <div className='hover:text-white delay-50  text-md font-semibold font-mono'>
+        
+            <Link href="/">Explore</Link></div>
+        <div className='hover:text-white delay-50  text-md font-semibold font-mono'>
+        
+            <Link href="/">Search</Link></div>
+
+            <div className='hover:text-white delay-50  text-md font-semibold font-mono'>
+        
+            <Link href="/post">Post</Link></div>
+
+        <div className='hover:text-white delay-50 text-md font-semibold font-mono '>
+        
+            <Link href="/">Profile</Link></div>
+              {/* <div className=' mx-2 my-2 '> */}            
+        <ConnectButton/>
+            </ul>
+        </nav>
+        {/* <hr/> */}
+        </>
+        
+        );
+        }
